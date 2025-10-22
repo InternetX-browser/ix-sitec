6ea63889ae3420876e7304155b50f1f8872c15e4
Nicolas Sokolov <nicosokolov2017@gmail.com>
2025-10-22 14:30:28 -0700

    Update 1.0.3

--- docs/src/components/App-header.js
+++ docs/src/components/App-header.js
@@ -8,22 +8,26 @@ const mainname2 = "Site Creator"
 const AppHeader = () => {
     return (
         <header><div className="HEAD-header">
-            <div className="logo">
             <LinkHome><img src="../ix-sitec-logo.png" width="64px" height="64px"/></LinkHome>
-            </div>
             <p>{mainname1}</p><br/>
-            <Link to="/"><p>{mainname2}</p></Link>
+            <LinkHome><p>{mainname2}</p></LinkHome>
             <div className="configure-figures">
                 <div className="figures">
-                <Link to="/create"><img src="./figure/create_repo.svg" alt="Create"/></Link>
+                <Link to="/create"><img src="./figure/add-box.svg" alt="Create" width="23px" height="23px"/></Link>
                 <Tooltip text="Wiki">
-                    <a href="https://github.com/InternetX-browser/ix-sitec/wiki/" ><img src="./figure/wiki.svg"/></a>
+                    <a href="https://github.com/InternetX-browser/ix-sitec/wiki/" ><img src="./figure/wiki-book.svg" width="30px" height="30px"/></a>
                 </Tooltip>
                 <Tooltip text="Source Code">
-                    <a href="https://github.com/InternetX-browser/ix-sitec"><img src="./figure/source-code.svg"/></a>
+                    <a href="https://github.com/InternetX-browser/ix-sitec"><img src="./figure/code.svg" width="23px" height="23px"/></a>
+                </Tooltip>
+                <Tooltip text="Group Messeger">
+                    <img src="../figure/group-messeger.svg" width="23px" height="23px" onClick={() => alert('In development!')} className="GM-Image"/>
                 </Tooltip>
                 </div>
             </div>
+                <div className="usr">
+                    <img src="../figure/user.svg" width="64px" height="64px"/><p>ShellUser</p><p>You are not using an account.</p>
+                </div>
         </div>
             </header>
     )
